43535fb1593ffbf332b70f55ee779b164a4e4c10
Brafalgar <74974195+Brafalgar@users.noreply.github.com>
2026-01-18 16:56:43 +0100

    (devel: some correction to deploy well 3

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,6 @@ const nextConfig = {
   output: 'export',
   basePath: '/Portfolio',
   assetPrefix: '/Portfolio/',
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'media.licdn.com',
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
